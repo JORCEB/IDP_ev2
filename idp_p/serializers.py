f0988ac4cfc1808b9ac06a488420e0cefa190708
JORCEB <jor.ceballos@duocuc.cl>
2024-06-11 16:45:24 -0400

    freefire

--- idp_p/serializers.py
+++ idp_p/serializers.py
@@ -4,40 +4,12 @@ from .models import Producto, HistorialPrecio, Inventario, Message, Conversation
 from django.contrib.auth.models import User
 
 class ProductoSerializer(serializers.ModelSerializer):
-    precio_convertido = serializers.SerializerMethodField()
+ 
 
     class Meta:
         model = Producto
-        fields = ['id', 'nombre', 'categoria', 'precio', 'descripcion', 'en_promocion', 'lanzamiento_reciente', 'precio_convertido']
+        fields = ['id', 'nombre', 'categoria', 'precio', 'descripcion', 'en_promocion', 'lanzamiento_reciente']
 
-    def get_precio_convertido(self, obj):
-        # Aquí deberías implementar la lógica para convertir el precio a la moneda deseada
-        # Puedes utilizar la API de conversión de divisas que prefieras
-        # Aquí un ejemplo de cómo podrías hacerlo con CurrencyStack
-
-        # Reemplaza 'YOUR_API_KEY' con tu clave de API real proporcionada por CurrencyStack
-        api_key = 'vd71sghc3jd7h597kmc2agmtjt'
-
-        # Moneda de origen (USD en este ejemplo)
-        moneda_origen = 'USD'
-        # Moneda de destino (la moneda a la que deseas convertir el precio)
-        moneda_destino = 'EUR'  # Por ejemplo, EUR para Euros
-
-        # Obtener el precio del producto
-        precio = obj.precio
-
-        # Realizar la solicitud a la API de conversión de divisas
-        url = f'https://api.currency.com/convert?access_key={api_key}&from={moneda_origen}&to={moneda_destino}&amount={precio}'
-        response = requests.get(url)
-        data = response.json()
-
-        # Verificar si la solicitud fue exitosa y obtener el precio convertido
-        if response.status_code == 200:
-            precio_convertido = data['result']
-            return precio_convertido
-        else:
-            # En caso de error, devolver el precio original sin realizar ninguna conversión
-            return precio
 
 class CrearProductoSerializer(serializers.ModelSerializer):
     class Meta:
